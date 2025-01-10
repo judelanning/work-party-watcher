ebc0e8762166979a19f3ae76e34099ccd7bd0257
Jude Lanning <judelanning15@gmail.com>
2025-01-09 22:36:52 -0800

    Adding base script

---
+++ script.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+base_url = "https://www.wta.org/volunteer/schedule/workparties.json?"
+
+response = requests.get(base_url)
+
+def get_events(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+
+            print(f"{len(data['results']['all_items'])} work parties fetched.")
+        except:
+            print("Did not receive valid JSON, aborting script now.")
+    
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+
+get_events(base_url)
